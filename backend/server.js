27ce18c90e810c72a575a539ed8122f08516216e
Marlon Mitchell <mdmitch7@gmail.com>
2023-10-05 11:17:49 -0400

    modified:   .gitignore 	modified:   backend/.env 	modified:   backend/package-lock.json 	modified:   backend/package.json 	modified:   backend/routes/workouts.js 	modified:   backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 const express = require('express');
-
+const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/workouts');
 
 // express app
 const app = express();
+
 
 //middleware
 app.use(express.json())
@@ -14,9 +15,20 @@ app.use((req, res, next) => {
 });
 
 // Routes
+// app.get('/', (req, res)=>{
+//  res.json(msg: 'Welcome to the app')
+//})
+
 app.use('/api/workouts', workoutRoutes);
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-  console.log('listening on port', process.env.PORT)
-});
+// connect to db
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log('connected to db & listening on port:',process.env.PORT)
+    })
+  })
+  .catch(err => console.log(err))
+
